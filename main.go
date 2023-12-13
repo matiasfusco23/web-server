087b5cbfed85bf05f846a7ad4f4bdbbce28b3aa3
matiasfusco23 <matias.fusco@mercadolibre.com>
2023-12-13 19:25:19 -0300

    Estructura de carpetas y refactor para separar funciones de endpoints

--- main.go
+++ main.go
@@ -57,109 +57,81 @@ type Persona struct {
 	Apellido string `json:"Apellido"`
 }
 
+func Ping(c *gin.Context) {
+	c.String(200, "pong")
+}
+func GetAllProducts(c *gin.Context) {
+	c.JSON(200, products)
+
+}
+func GetProductById(c *gin.Context) {
+	for _, product := range products {
+		param_id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.String(404, "Product not found")
+			return
+
+		}
+		if product.Id == param_id {
+			c.JSON(200, product)
+			return
+		}
+
+	}
+	c.String(404, "Product not found")
+
+}
+
+func GetProductsWithPriceGreaterThan(c *gin.Context) {
+	var products_with_desired_price []Product
+	for _, product := range products {
+		min_price, err := strconv.ParseFloat(c.Query("min_price"), 64)
+
+		if err != nil {
+			c.String(404, "Please provide a valid price")
+			return
+		}
+
+		if product.Price > min_price {
+			products_with_desired_price = append(products_with_desired_price, product)
+		}
+
+	}
+	c.JSON(200, products_with_desired_price)
+}
+
+func CreateProduct(c *gin.Context) {
+	var request ProductCreation
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	lastIndex := len(products) - 1
+	lastProductId := products[lastIndex].Id
+	//TODO:
+	/*
+		Ningún dato puede estar vacío, exceptuando is_published (vacío indica un valor false).
+		El campo code_value debe ser único para cada producto.
+		La fecha de vencimiento debe tener el formato: XX/XX/XXXX, además debemos verificar que día, mes y año sean valores válidos.
+	*/
+
+	newProduct := Product{lastProductId + 1, request.Name, request.Quantity, request.CodeValue, request.IsPublished, request.Expiration, request.Price}
+	products = append(products, newProduct)
+	c.String(200, "Product added!")
+}
 func main() {
 	router := gin.Default()
 	populate_products("products.json")
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+	router.GET("/ping", Ping)
 
-	router.POST("/saludo", func(c *gin.Context) {
-		reqBody := new(Persona)
-		err := c.BindJSON(&reqBody)
-		if err != nil {
-			fmt.Println("error: ", err.Error())
-			c.String(400, fmt.Sprintf("Error in body: %s err", err.Error()))
-			return
-		}
-		resultado := fmt.Sprintf("Hola %s %s", reqBody.Nombre, reqBody.Apellido)
-		c.String(200, resultado)
-		fmt.Println("Ejecucion exitosa")
+	router.GET("products", GetAllProducts)
 
-	})
+	router.GET("products/:id", GetProductById)
 
-	//puedo definir endpoints agrupados
-	grupo := router.Group("/perfil")
-	grupo.GET("/foto")
-	grupo.GET("/info")
+	router.GET("products/withPriceGreaterThan", GetProductsWithPriceGreaterThan)
 
-	var empleados = map[string]string{
-		"1": "pepito",
-		"2": "jaimito",
-	}
-
-	router.GET("empleados/:id", func(c *gin.Context) {
-		empleado, ok := empleados[c.Param("id")]
-		nombre := c.Query("nombre")
-		fmt.Println("Id param received: ", c.Param("id"))
-		if ok && nombre == empleado {
-			c.String(200, "Nombre: %s, ID: %s", empleado, c.Param("id"))
-		} else {
-			c.String(404, "No se encontro el empleado, por favor dar su nombre por query param")
-		}
-	})
-
-	router.GET("products", func(c *gin.Context) {
-		c.JSON(200, products)
-
-	})
-
-	router.GET("products/:id", func(c *gin.Context) {
-		for _, product := range products {
-			param_id, err := strconv.Atoi(c.Param("id"))
-			if err != nil {
-				c.String(404, "Product not found")
-				return
-
-			}
-			if product.Id == param_id {
-				c.JSON(200, product)
-				return
-			}
-
-		}
-		c.String(404, "Product not found")
-
-	})
-
-	router.GET("products/withPriceGreaterThan", func(c *gin.Context) {
-		var products_with_desired_price []Product
-		for _, product := range products {
-			min_price, err := strconv.ParseFloat(c.Query("min_price"), 64)
-
-			if err != nil {
-				c.String(404, "Please provide a valid price")
-				return
-			}
-
-			if product.Price > min_price {
-				products_with_desired_price = append(products_with_desired_price, product)
-			}
-
-		}
-		c.JSON(200, products_with_desired_price)
-	})
-
-	router.POST("products/", func(c *gin.Context) {
-		var request ProductCreation
-		if err := c.ShouldBindJSON(&request); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		lastIndex := len(products) - 1
-		lastProductId := products[lastIndex].Id
-		//TODO:
-		/*
-			Ningún dato puede estar vacío, exceptuando is_published (vacío indica un valor false).
-			El campo code_value debe ser único para cada producto.
-			La fecha de vencimiento debe tener el formato: XX/XX/XXXX, además debemos verificar que día, mes y año sean valores válidos.
-		*/
-
-		newProduct := Product{lastProductId + 1, request.Name, request.Quantity, request.CodeValue, request.IsPublished, request.Expiration, request.Price}
-		products = append(products, newProduct)
-		c.String(200, "Product added!")
-	})
+	router.POST("products/", CreateProduct)
 
 	router.Run(":8081")
 }
