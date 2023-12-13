268c5055c1d191c3ea2bccd06f8914804922a131
matiasfusco23 <matias.fusco@mercadolibre.com>
2023-12-13 19:15:46 -0300

    POST para creacion de productos

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"strconv"
 
@@ -13,6 +14,15 @@ import (
 
 type Product struct {
 	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Quantity    int     `json:"quantity"`
+	CodeValue   string  `json:"code_value"`
+	IsPublished bool    `json:"is_published"`
+	Expiration  string  `json:"expiration"`
+	Price       float64 `json:"price"`
+}
+
+type ProductCreation struct {
 	Name        string  `json:"name"`
 	Quantity    int     `json:"quantity"`
 	CodeValue   string  `json:"code_value"`
@@ -129,6 +139,26 @@ func main() {
 
 		}
 		c.JSON(200, products_with_desired_price)
+	})
+
+	router.POST("products/", func(c *gin.Context) {
+		var request ProductCreation
+		if err := c.ShouldBindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		lastIndex := len(products) - 1
+		lastProductId := products[lastIndex].Id
+		//TODO:
+		/*
+			Ningún dato puede estar vacío, exceptuando is_published (vacío indica un valor false).
+			El campo code_value debe ser único para cada producto.
+			La fecha de vencimiento debe tener el formato: XX/XX/XXXX, además debemos verificar que día, mes y año sean valores válidos.
+		*/
+
+		newProduct := Product{lastProductId + 1, request.Name, request.Quantity, request.CodeValue, request.IsPublished, request.Expiration, request.Price}
+		products = append(products, newProduct)
+		c.String(200, "Product added!")
 	})
 
 	router.Run(":8081")
